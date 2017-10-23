2d16d55d2f0b6a84a910d2f225fe55b452efb6fb
taruc <taruc@B79>
2017-10-23 12:15:35 +0800

    My first local commit

---
+++ src/f10/F10.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package f10;
+
+/**
+ *
+ * @author taruc
+ */
+public class F10 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+    
+}
